e76b1f940512fb0762668fc6dbdc3653a6c3c474
huutribui <huu-tri.bui@intel.com>
2024-03-20 15:56:21 -0700

    Inititialized routes and pages

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './pages/HomePage';
+import Notifications from './pages/Notifications';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/notifications" component={Notifications} />
+        </Switch>
+      </Router>
     </div>
   );
 }
