d1b61fc4b6a9578575f745b6fd8553916fc24ccc
huutribui <huu-tri.bui@intel.com>
2024-03-20 16:44:18 -0700

    Inititialized navbar and context provider

---
+++ react-app/src/components/InfoProvider.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+
+export const InfoContext = React.createContext();
+
+let MOCK_DATA = [
+    {
+        "id": 0,
+        "type": "Billing",
+        "message": "Your cloud credits will expire soon. Only $50 left.",
+        "date": new Date() - 500000,
+        "status" : "new"
+    },
+    {
+        "id": 1,
+        "type": "Billing",
+        "message": "Your cloud credits will expire soon. Only $50 left.",
+        "date": new Date(),
+        "status" : "new"
+    },
+    {
+        "id": 2,
+        "type": "EC2",
+        "message": "Your instance \"learning-server\" is provisioned and running.",
+        "date": new Date(),
+        "status" : "new"
+    },
+    {
+        "id": 3,
+        "type": "newProduct",
+        "message": "Run you AI-processing with our new Habana Gaudi 2 instances.",
+        "date": new Date(),
+        "status" : "new"
+    },
+    {
+        "id": 4,
+        "type": "downtime",
+        "message": "Intel Developer Could instances will be unavailable from 2:00am to 2:30am on June 2nd.",
+        "date": new Date(),
+        "status" : "new"
+    },
+]
+
+const InfoProvider = ({ children }) => {
+    const [notifications, setNotifications] = useState(MOCK_DATA);
+
+    const removeNotification = (id) => {
+        console.log("removed noti:", id);
+    }
+
+    const viewedNotification = (id) => {
+        console.log("viewed noti: ", id);
+    }
+    
+    return (
+		<InfoContext.Provider
+			value={{
+				notifications: notifications,
+				viewedNotification: viewedNotification,
+				removeNotification: removeNotification,
+			}}
+		>
+			{children}
+		</InfoContext.Provider>
+	);
+}
+export default InfoProvider;
